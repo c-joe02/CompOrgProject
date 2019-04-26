66840fc96a9ed8557ad28c45cdf6085a55af2e03
miggy808 <33299350+miggy808@users.noreply.github.com>
2019-04-25 14:44:26 -1000

    Update Processor.java

--- Processor.java
+++ Processor.java
@@ -49,6 +49,81 @@ public class Processor {
     
     
     public void decode(){
+        int pab =1;
+        //1. load a b // reg[a] = cell[reg[b]]
+        switch(pab){
+            case 1:
+               //insert code here -Miguel
+                
+                break;
+        //2. loadc a // reg[a] = cell[PC++]
+            case 2:
+                //insert code here -Miguel
+               
+                break;
+        //3. store a b // cell[reg[a]] = reg[b]
+            case 3:
+                //insert code here -Miguel
+                
+                break;
+        //4. add a b // reg[a] = reg[a] + reg[b]        
+            case 4:
+                //insert code here -Miguel
+                
+                break;
+        //5. mul a b // reg[a] = reg[a] * reg[b]
+            case 5:
+                //insert code here -Miguel
+                
+                break;
+        //6. sub a b // reg[a] = reg[a] - reg[b]
+            case 6:
+                //insert code here -Miguel
+               
+                break;
+        //7. div a b // reg[a] = reg[a] / reg[b], error if reg[b] == 0
+            case 7:
+                //insert code here -Miguel
+                
+                break;    
+        //8. and a b // if (reg[a]!= 0&&reg[b]!=0) reg[a]=1 else reg[a]=0
+            case 8:
+                //insert code here -Miguel
+                
+                break;
+        //9. or a b // if (reg[a]!=0||reg[b]!=0) reg[a]=1 else reg[a]=0
+            case 9:
+                //insert code here -Miguel
+                     
+                break;
+        //10. not a b // if (reg[b]!=0) reg[a]=0 else reg[a]=1
+            case 10:
+                
+                break;
+        //11. lshift a b // reg[a] = reg[b] << 1
+            case 11:
+                
+                break;
+        //12. rshift a b // reg[a] = reg[b] >> 1
+            case 12:
+                
+                break;
+        //13. bwc a b // reg[a] = reg[a] & reg[b]
+            case 13:
+                
+                break;
+        //14. bwd a b // reg[a] = reg[a] | reg[b]
+            case 14:
+                
+                break;
+        //15. if a b // if (reg[a] != 0) pc = reg[b]
+            case 15:
+                
+                break;
+        //0. halt // stop fetch-execute cycle
+            default:
+            
+        }
         
     }
     public int getPC() {
